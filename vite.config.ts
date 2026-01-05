0c36ed5b9ec0cdb65daae41efb16547bd5fc585d
Abandoned Cart <twistedumbrella@gmail.com>
2026-01-05 13:46:31 -0500

    Use android virtual asset path for base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SegmentEditor/',
+  base: 'https://appassets.androidplatform.net/assets/dist/',
   plugins: [
     devtools(),
     tanstackRouter({
