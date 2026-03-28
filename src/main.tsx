7aa015d9e5271ad7d4e6028438c1ed4eb7b7b1d3
rlauuzo <46294892+rlauuzo@users.noreply.github.com>
2026-03-28 18:03:49 +0100

    Rename root element id to segment-editor-root

--- src/main.tsx
+++ src/main.tsx
@@ -84,7 +84,7 @@ declare module '@tanstack/react-router' {
 }
 
 // Render the app
-const rootElement = document.getElementById('app')
+const rootElement = document.getElementById('segment-editor-root')
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
